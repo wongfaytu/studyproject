6fbd8026bc1b481f770a548d43266ee473cc5dd0
wongfaytu <>
2018-08-01 23:57:54 +0900

    初期化

--- Fragment3/app/src/main/java/com/example/satoryuji/fragment/MainActivity.java
+++ Fragment3/app/src/main/java/com/example/satoryuji/fragment/MainActivity.java
@@ -12,15 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Fragmentを作成します
-        MainFragment fragment = new MainFragment();
-        // Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        // 新しく追加を行うのでaddを使用します
-        // 他にも、よく使う操作で、replace removeといったメソッドがあります
-        // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
-        transaction.add(R.id.container, fragment);
-        // 最後にcommitを使用することで変更を反映します
-        transaction.commit();
     }
 }
